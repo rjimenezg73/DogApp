d49356b4f149ff58aebd10fe155ff990e81e32bb
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-09-12 18:12:01 -0500

    modelado dog y temperament

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -3,10 +3,43 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    'dog',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      height:{
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      weight: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      life_span: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      created_DB: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      }
     },
-  });
+    { timestamps: false }
+  );
 };
+
+
---
+++ api/src/models/Temperament.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+  sequelize.define(
+    'temperament', 
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    }, 
+    { timestamps: false }
+  );
+};
