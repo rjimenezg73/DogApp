fb4fcf4955d93323bf649152f915ed08fa9bf55b
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-09-12 20:57:00 -0500

    relacion mucho a muchos entre ambas tablas del proyecto

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog } = sequelize.models;
+const { Dog, Temperaments } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Dog.belongsToMany(Temperaments, { through: 'dog_temperaments', timestamps: false })
+Temperaments.belongsToMany(Dog, { through: 'dog_temperaments', timestamps: false })
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Temperaments.js
+++ api/src/models/Temperaments.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define(
-    'temperament', 
+    'temperaments', 
     {
       name: {
         type: DataTypes.STRING,
